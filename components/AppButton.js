28abb0ef7e40573563fb5a33ebcff3cb372453e1
Thanh Nguyen <huuthansv@gmail.com>
2024-03-15 08:04:11 +0700

    Ep6: Component Props

--- components/AppButton.js
+++ components/AppButton.js
@@ -1,8 +1,15 @@
 export default {
+    props: {
+        variant: {
+            type: String,
+            required: false,
+        },
+    },
+
     template: `
         <div>
             <h2>what's up</h2>
-            <button :disabled="processing">Click me</button>
+            <button :disabled="processing" :type="variant">Click me</button>
         </div>
     `,
     mounted() {
